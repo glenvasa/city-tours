f9ff21f842ff21d8c24951f606c0ebe0e158cca4
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-12-30 15:09:10 -0500

    created handleInfo method to toggle display of tour info

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,30 +1,42 @@
 import React, { Component } from "react";
 import "./Tour.scss";
-import photo from "./pexels-pixabay-40142.jpg";
 
 export default class Tour extends Component {
+  state = {
+    showInfo: false,
+  };
+
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
+  };
   render() {
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
     return (
       <article className="tour">
         <div className="img-container">
-          <img src={photo} alt="boston-skyline" />
-          <span className="close-btn">
+          <img src={img} alt="boston-skyline" />
+          <span
+            className="close-btn"
+            onClick={() => {
+              removeTour(id);
+            }}
+          >
             <i className="fas fa-window-close" />
           </span>
         </div>
         <div className="tour-info">
-          <h3>city</h3>
-          <h4>name</h4>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
           <h5>
-            info{" "}
-            <span>
+            info
+            <span onClick={this.handleInfo}>
               <i className="fas fa-caret-square-down"></i>
             </span>
           </h5>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
-            debitis.
-          </p>
+          {this.state.showInfo && <p>{info}</p>}
         </div>
       </article>
     );
